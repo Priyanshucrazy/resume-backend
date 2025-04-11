d4fcd9028a0013dc29210bc00ab614e1d209207e
Priyanshucrazy <112460693+Priyanshucrazy@users.noreply.github.com>
2025-04-11 18:53:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ from dotenv import load_dotenv
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
+@app.route("/")
+def home():
+    return "✅ Resume Scanner Backend is Running!"
+
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
